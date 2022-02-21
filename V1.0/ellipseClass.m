0062b9ed7c291d64c9a0425e54db4951fc778aab
umroot <umroot@collar.encs.concordia.ca>
2022-02-21 18:32:36 +0000

    Added all dependant classes such as ellipseClass.m in V1.0

---
+++ V1.0/ellipseClass.m
@@ -0,0 +1,22 @@
+classdef ellipseClass
+    properties
+        x_center
+        y_center
+        radius_major
+        radius_minor
+    end
+    methods
+        function obj = ellipseClass(x, y, R, r)
+            obj.x_center = x;
+            obj.y_center = y;
+            obj.radius_major = R;
+            obj.radius_minor = r;
+        end
+        
+        function bool = inEllipse(obj, x, y)
+            bool = (((x - obj.x_center) / obj.radius_major) ^ 2 + ((y - obj.y_center) / obj.radius_minor) ^ 2) <= 1;
+        end
+    end
+end
+
+        
---
+++ V1.0/get_circle.m
@@ -0,0 +1,40 @@
+function [coord_excess] = get_circle(X, Y, R, dx, midCircle)
+    
+    x = X - R;
+    y = Y + (R ^ 2 - (x - X) ^ 2) ^ 0.5;
+    total = 0;
+    while (x < X + R)
+        segment_line = 2 * y;
+        num_div_y = segment_line / dx;
+        total = total + floor(num_div_y);
+        x = x + dx;
+        y = Y + (R ^ 2 - (x - X) ^ 2) ^ 0.5;
+    end
+    %%
+    coord_excess = zeros(total, 2);
+    x = X - R;
+    y = Y + (R ^ 2 - (x - X) ^ 2) ^ 0.5;
+    nnum = 0;
+
+    while (x < X + R)
+        segment_line = 2 * y;
+        num_div_y = segment_line / dx;
+        for j = 1: num_div_y
+            coordx = x;
+            coordy = (-1) * y + (j - 1) * dx;
+             if (midCircle.inEllipse(coordx, coordy))
+                 continue;
+             end
+            
+            nnum = nnum + 1;
+            coord_excess(nnum, 1) = coordx;
+            coord_excess(nnum, 2) = coordy;
+
+        end
+        x = x + dx;
+        y = Y + (R ^ 2 - (x - X) ^ 2) ^ 0.5; 
+
+    end
+    coord_excess = coord_excess(1:nnum, :);
+end
+
