d85e98ef6d33e90406546da23e12b6b47b89ed28
umroot <umroot@collar.encs.concordia.ca>
2022-05-26 18:23:28 +0100

    Stress: added reactionary force to the total forces in stress calculation by modifying the summations done in nodefam at the end of each main iteration. Plus localThickness is added to CalculateStress function instead of thick to account for variable widths

--- V1.0/CalculateStressforPoint.m
+++ V1.0/CalculateStressforPoint.m
@@ -1,6 +1,6 @@
 %%%The function below calculates the stresses in the x and y direction of
 %%%all the given material points and returns a stress array of size = [totalNumMatPoint, 2].
-function [stress] = CalculateStressforPoint(coord,TotalNumMatPoint,numfam,nodefam, pointfam, thick)
+function [stress] = CalculateStressforPoint(coord,TotalNumMatPoint,numfam,nodefam, pointfam, Deltas)
     stress = zeros(TotalNumMatPoint, 2); %Predifining the size of the stress array to speed up the calculations.
 
     for i = 1: (TotalNumMatPoint)
@@ -8,30 +8,31 @@ function [stress] = CalculateStressforPoint(coord,TotalNumMatPoint,numfam,nodefa
         f_y = 0; %Sum of all forces in Y direction
         satisfy_x = 0;
         satisfy_y = 0;
+        localThickness = Deltas(i,1) / 3.015 / 2;
         point_x = coord(i,1); %% Extract coordinates of point i
         point_y = coord(i,2);
         for j = 1: TotalNumMatPoint %% Iterating every other material point for conditions
-            if (coord(j,2) == point_y && coord(j,1) < point_x + thick / 2) %% coord(j,2) == point_y &&
+            if (coord(j,2) == point_y && coord(j,1) < point_x + localThickness / 2) %% coord(j,2) == point_y &&
                     satisfy_x = satisfy_x + 1;
                     for k = 1:numfam(j,1) %%Iterate for all the neighbors of point j that satisfy the conditions.
                         cnode = nodefam(pointfam(j,1)+k-1,1);
-                        if (coord(cnode,1) > point_x + thick / 2) % Sum bondforce of the neighbors that satisfy this condition
+                        if (coord(cnode,1) > point_x + localThickness / 2) % Sum bondforce of the neighbors that satisfy this condition
                             f_x = f_x + nodefam(pointfam(j,1)+k-1, 2); %%% Requires Saving BondForce_x in nodefam(:,2)
                         end
                     end
             end
             %Same below for stress in y direction
-            if (coord(j,1) == point_x && coord(j,2) < point_y + thick / 2) %%%%coord(j,1) == point_x &&
+            if (coord(j,1) == point_x && coord(j,2) < point_y + localThickness / 2) %%%%coord(j,1) == point_x &&
                     satisfy_y = satisfy_y + 1;
                     for k = 1:numfam(j,1) %%Iterate for all the neighbors of point j that satisfy the conditions.
                         cnode = nodefam(pointfam(j,1)+k-1,1);
-                        if (coord(cnode,2) > point_y + thick / 2) % Sum bondforce of the neighbors that satisfy this condition
+                        if (coord(cnode,2) > point_y + localThickness / 2) % Sum bondforce of the neighbors that satisfy this condition
                             f_y = f_y + nodefam(pointfam(j,1)+k-1, 3); %%% Requires Saving BondForce_x in nodefam(:,2)
                         end
                     end
             end
-            stress_x = f_x * thick;
-            stress_y = f_y * thick;
+            stress_x = f_x * localThickness;
+            stress_y = f_y * localThickness;
         end
         %Saving the stress for point i before moving on to the next point
         %on the plate
